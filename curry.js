c62288b41013ca35e15352b057c20640279258b0
Ayush Srivastava <ayushs.1992@gmail.com>
2020-05-01 09:14:31 +0530

    Updated curry.

--- curry.js
+++ curry.js
@@ -1,7 +1,7 @@
 function curry(fn) {
     return function(...args) {
       if (args.length >= fn.length) {
-        return fn.apply(this);
+        return fn.apply(this, args);
       }
       return function(...newArgs) {
         return curry.apply(this, [...args, ...newArgs]);
