de949843badf05438a91bc2c6a098fc68af0b4fd
Shubham Kulkarni <58355613+shubhamsk894@users.noreply.github.com>
2020-05-10 12:02:25 +0530

    get value from given object path

---
+++ getPathValue.js
@@ -0,0 +1,42 @@
+export const getPathFromString = str => {
+
+    if (typeof str !== 'string') return path;
+    let res = [];
+	str.split('.').forEach(item => {
+    item.split(/\[([^}]+)\]/g).forEach( i=> {if (i.length > 0) res.push(i)})
+        })
+	return res;
+
+}
+
+export const  getPathValue = (obj, objPath, defaultVal) => {
+
+	objPath = getPathFromString(objPath);
+	let currObject = obj;
+
+
+	for (i = 0; i < objPath.length; i++) {
+		if (!currObject[objPath[i]]) return defaultVal;	
+		currObject = currObject[objPath[i]];
+	}
+	return currObject;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
