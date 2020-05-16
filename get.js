56034555dc4bff8a10ef1692ebe65bf41ccbd0f5
Ayush Srivastava <ayushs.1992@gmail.com>
2020-05-17 00:27:43 +0530

    Removed whitespace.

--- get.js
+++ get.js
@@ -1,6 +1,6 @@
 import pathFromString from "./.internal/pathFromString.js";
 
-export const  get = (obj, objPath, defaultVal) => {
+export const get = (obj, objPath, defaultVal) => {
 
 	objPath = pathFromString(objPath);
 	let currObject = obj;
