54a4548b7ac6dd206d5214b267d5a0e814f1879b
Ayush Srivastava <ayushs.1992@gmail.com>
2020-05-17 00:27:21 +0530

    moved internal function to .internal folder.

--- get.js
+++ get.js
@@ -1,17 +1,8 @@
-export const getPathFromString = str => {
-
-    if (typeof str !== 'string') return path;
-    let res = [];
-	str.split('.').forEach(item => {
-    item.split(/\[([^}]+)\]/g).forEach( i=> {if (i.length > 0) res.push(i)})
-        })
-	return res;
-
-}
+import pathFromString from "./.internal/pathFromString.js";
 
 export const  get = (obj, objPath, defaultVal) => {
 
-	objPath = getPathFromString(objPath);
+	objPath = pathFromString(objPath);
 	let currObject = obj;
 
 
