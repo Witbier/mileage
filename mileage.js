26323b5240a710a605d790a4713ecc25f3db48f0
Ruchita Karwa <ruchitakarwa9@gmail.com>
2020-05-18 16:34:11 +0530

    Added head & chunk

--- mileage.js
+++ mileage.js
@@ -1,7 +1,11 @@
 import curry from './curry';
 import compose from './compose';
+import head from './head';
+import chunk from './chunk';
 
 export default {
     curry,
-    compose
+    compose,
+    head,
+    chunk
 };
