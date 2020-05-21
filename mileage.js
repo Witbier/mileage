f7a0928b233d5ca99ff0b65007c6ea3198cfa7e4
Ruchita Karwa <ruchitakarwa9@gmail.com>
2020-05-21 13:22:01 +0530

    Update mileage.js
    Added throttle

--- mileage.js
+++ mileage.js
@@ -2,10 +2,12 @@ import curry from './curry';
 import compose from './compose';
 import head from './head';
 import chunk from './chunk';
+import throttle from './throttle';
 
 export default {
     curry,
     compose,
     head,
-    chunk
+    chunk,
+    throttle
 };
